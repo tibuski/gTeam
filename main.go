419c452c0ef014d632b34594e1899afc0f8afb2c
tibuski <laurent.brichet@gmail.com>
2024-05-28 14:44:28 +0200

    added days

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 type Employee struct {
 	Name  string
@@ -8,7 +11,24 @@ type Employee struct {
 	Team  string
 }
 
+type Calendar struct {
+	Name string
+	Days []int
+}
+
 var Employees = []Employee{}
+
+func daysOfTheMonth(month time.Month, year int) []int {
+	var days []int
+	start := time.Date(year, month, 1, 0, 0, 0, 0, time.UTC)
+	fmt.Println(start)
+	end := start.AddDate(0, 1, -1)
+	fmt.Println(end)
+	for t := start; !t.After(end); t = t.AddDate(0, 0, 1) {
+		days = append(days, int(t.Weekday()))
+	}
+	return days
+}
 
 func addEmployee(n, e, t string) Employee {
 	human := Employee{Name: n, Email: e, Team: t}
@@ -16,8 +36,15 @@ func addEmployee(n, e, t string) Employee {
 }
 
 func main() {
-	Employees = append(Employees, addEmployee("Laurent", "l@b.be", "CVVS"))
-	Employees = append(Employees, addEmployee("Gerard", "t@po.fr", "BCNS"))
 
-	fmt.Println(Employees)
+	Employees = append(Employees, addEmployee("Laurent", "laurent@brichet.be", "CVVS"))
+	Employees = append(Employees, addEmployee("Gerard", "gerard@proximus.lu", "BCNS"))
+	Employees = append(Employees, addEmployee("Pol", "pol.dupont@proximus.lu", "CVVS"))
+	Employees = append(Employees, addEmployee("Roger", "Roro.Gege@proximus.lu", "BCNS"))
+
+	NumberOfDays := daysOfTheMonth(5, 2024)
+	// fmt.Println(Employees)
+	fmt.Println(len(NumberOfDays))
+	fmt.Println(NumberOfDays)
+
 }
