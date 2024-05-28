c49fad5035b6efa0a16d13c94b81aa51c6cfb43a
tibuski <laurent.brichet@gmail.com>
2024-05-28 15:54:52 +0200

    next sqlite

--- main.go
+++ main.go
@@ -13,18 +13,22 @@ type Employee struct {
 
 type Calendar struct {
 	Name string
-	Days []int
+	Days []string
 }
 
 var Employees = []Employee{}
 var TeamCalendar = []Calendar{}
 
-func daysOfTheMonth(month int, year int) []int {
-	var days []int
+func daysOfTheMonth(month int, year int) []string {
+	var days []string
 	start := time.Date(year, time.Month(month), 1, 0, 0, 0, 0, time.UTC)
 	end := start.AddDate(0, 1, -1)
 	for t := start; !t.After(end); t = t.AddDate(0, 0, 1) {
-		days = append(days, int(t.Weekday()))
+		if t.Weekday() == 6 || t.Weekday() == 0 {
+			days = append(days, "W")
+		} else {
+			days = append(days, "0")
+		}
 	}
 	return days
 }
